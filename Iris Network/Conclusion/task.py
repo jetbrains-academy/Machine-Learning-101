397200e2a786e8eb88260047beac9f5607d51e54
sofiiako <19rubisco93@gmail.com>
2022-01-12 15:23:02 +0300

    changed name, removed remote info files

--- Iris Network/Conclusion/task.py
+++ Iris Network/Conclusion/task.py
@@ -29,7 +29,6 @@ def train_test_split(X, y, ratio=0.8):
     return X[indices[:train_len]], y[indices[:train_len]], X[indices[train_len:]], y[indices[train_len:]]
 
 
-
 if __name__ == '__main__':
     X, y = read_data('iris.csv')
     # comment the following line if you don't need the plot anymore
