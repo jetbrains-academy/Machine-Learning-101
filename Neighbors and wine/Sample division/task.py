5d9c82ea5d4b8c90f65e39b9f820de93dcc8573c
aleksandra.valerianova <sasha.val2006@gmail.com>
2025-12-07 16:48:16 +0200

    Neighbors and wine placeholders are done

--- Neighbors and wine/Sample division/task.py
+++ Neighbors and wine/Sample division/task.py
@@ -6,16 +6,16 @@ def train_test_split(X, y, ratio=0.8):
     train_len = int(X.shape[0] * ratio)
     # A slice with first train_len elements of X will be
     # the training sample for objects.
-    X_train
+    X_train = X[indices[:train_len]]
     # A slice with the rest of X will be
     # the testing sample for objects.
-    X_test
+    X_test = X[indices[train_len:]]
     # A slice with first train_len of y will be
     # the training sample for classes.
-    y_train
+    y_train = y[indices[:train_len]]
     # A slice with the rest of y will be
     # the testing sample for classes.
-    y_test
+    y_test = y[indices[train_len:]]
     return X_train, y_train, X_test, y_test
 
 
