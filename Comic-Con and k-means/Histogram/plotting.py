49ef5de7da4fe04c00124fca2e84a57f6807b1ac
aleksandra.valerianova <sasha.val2006@gmail.com>
2026-02-19 14:39:04 +0200

    Corrected placeholders and added comments

--- Comic-Con and k-means/Histogram/plotting.py
+++ Comic-Con and k-means/Histogram/plotting.py
@@ -3,20 +3,40 @@ from PIL import Image, ImageDraw
 
 
 def centroid_histogram(labels):
+    # Here we retrieve the counts of unique entries in the
+    # labels array (those are the clusters, and we need to know how many
+    # objects belong to each of them).
     unique, counts = np.unique(labels, return_counts=True)
     return counts
 
 
+# This function will allow us to draw the clusters received from the
+# k-means as bars in a histogram.
 def plot_colors(hist, centroids):
+    # The bar is an array representing the underlying rectangle behind
+    # the histogram. It is scaled to have a width of 500.
     bar = np.zeros((50, 500, 3), dtype=np.uint8)
+    # We create an Image object from the array.
     hist_plot = Image.fromarray(bar)
+    # We create a drawing instance of the hist_plot.
     draw = ImageDraw.ImageDraw(hist_plot)
-
+    # The x-axis position where we start to draw
     start_x = 0
+    # The overall sum of the samples belonging to all the clusters
     sum_hist = np.sum(hist)
-    for (percent, color) in zip(hist, centroids):
-        end_x = start_x + percent * 500 / sum_hist
-        draw.rectangle(((int(start_x), 0), (int(end_x), 50)), tuple(color))
-        start_x = end_x
 
+    for (percent, color) in zip(hist, centroids):
+        # The ending x-axis position is obtained
+        # by adding the percentage of all cluster points
+        # of a given color in the overall count
+        # (scaled to have width 500)
+        # to the starting position.
+        end_x = start_x + percent * 500 / sum_hist
+        # Here we draw the current rectangle with a height of 50 and color
+        # defined by the clusters' centroid.
+        draw.rectangle(((int(start_x), 0), (int(end_x), 50)), tuple(color))
+        # We set the starting point for the next bar to the ending point
+        # of the current one
+        start_x = end_x
+    # We save the image as a .png.
     hist_plot.save("histogram.png")
--- Comic-Con and k-means/Image recoloring/processing.py
+++ Comic-Con and k-means/Image recoloring/processing.py
@@ -9,10 +9,14 @@ IMAGE_HEIGHT = 1024
 
 
 def recolor(image, n_colors):
+    # Here we bring everything together by calling the k_means function on the image
+    # and providing the number of colors in which it needs to be recolored.
+    # Note that the image array is better unified by calling .astype(np.int64) on it.
     (labels, centroids) = k_means(image.astype(np.int64), n_colors, euclidean_distance)
     return centroids[labels]
 
 
+# Call this function inside the main method in task.py to recolor the image!
 def process_image(image):
     recolored_image = recolor(image, 8).reshape(IMAGE_HEIGHT, IMAGE_WIDTH, 3).astype('uint8')
     image = Image.fromarray(recolored_image)
